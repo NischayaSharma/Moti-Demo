ac1812b493411e68c936532841ebb5ca808bee10
Nischaya Sharma <45327263+NischayaSharma@users.noreply.github.com>
2021-03-12 21:28:48 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,40 @@
+
+import React from 'react';
+import { Dimensions, Pressable, StatusBar, StyleSheet,  } from 'react-native';
+import { useAnimationState, View as MotiView } from 'moti';
+
+const {width, height} = Dimensions.get("screen")
+export default function App() {
+  const [pressed, setPressed] = React.useState(true)
+  return (
+    <Pressable 
+      style={styles.container}
+      onPress={() => {
+        console.log(pressed);
+        setPressed(!pressed);
+      }}>
+      <MotiView
+        animate={{
+          translateY: pressed ? 100:0,
+          delay: 2000,
+        }}
+        style={styles.shape}
+      />
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  shape: {
+    height: 250,
+    width: 250,
+    backgroundColor: "blue",
+    borderRadius: 50,
+  }
+})
