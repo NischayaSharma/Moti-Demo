c3eb847a27b7d02972a07e85c3a50b6495405238
Nischaya Sharma <45327263+NischayaSharma@users.noreply.github.com>
2021-03-13 22:04:11 +0530

    fix: Working

--- App.js
+++ App.js
@@ -10,7 +10,6 @@ export default function App() {
     <Pressable 
       style={styles.container}
       onPress={() => {
-        console.log(pressed);
         setPressed(!pressed);
       }}>
       <MotiView
